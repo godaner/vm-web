513f8d2a3e091716abfef194644f675bf8d48475
zhangke <zhangke@wezyun.com>
2018-01-17 09:33:54 +0800

    add frontend code

--- src/backend/run_server.bat
+++
@@ -1,3 +0,0 @@
-::[1]:稳定插件分离打包,[2]:进行项目编译打包,[3]启动静态服务器
-nodemon static_server.js
-pause
--- src/backend/static_server.js
+++ src/backend/static_server.js
@@ -21,8 +21,8 @@ app.engine('html', ejs.renderFile);
 // 静态文件配置
 app.use('/', express.static(path.join(__dirname, './')));
 
-// 启动一个服务，监听从3000端口进入的所有连接请求
-var server = app.listen(3000, function(){
+// 启动一个服务，监听进入的所有连接请求
+var server = app.listen(3999, function(){
     var host = server.address().address;
     var port = server.address().port;
     console.log('Listening at http://%s:%s', host, port);
