d22eaa3749a6da412c8f135abb4ef86ba30d526a
zhangke <zhangke@wezyun.com>
2018-01-17 18:33:19 +0800

    fuck project

--- src/vm-frontend/js/vm_config.js
+++ src/vm-frontend/js/vm_config.js
@@ -1,6 +1,6 @@
 var vm_config = {
     //http前缀
-    http_url_prefix:"http://localhost:8888/",
+    http_url_prefix:"http://localhost:8888",
     //websocket前缀
     ws_url_prefix:"ws://localhost:8888"
 };
