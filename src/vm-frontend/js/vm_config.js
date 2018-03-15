3ab91c2807ffba297f4691286db473f63fa3d9d9
zhangke <zhangke@wezyun.com>
2018-03-15 16:06:12 +0800

    配置网关地址

--- src/vm-frontend/js/vm_config.js
+++ src/vm-frontend/js/vm_config.js
@@ -1,6 +1,6 @@
 var vm_config = {
     //http前缀
-    http_url_prefix:"http://192.168.11.222:5551",
+    http_url_prefix:"http://192.168.0.122:5551",
     //websocket前缀
     ws_url_prefix:"ws://localhost:5551",
     //受保护的页面
