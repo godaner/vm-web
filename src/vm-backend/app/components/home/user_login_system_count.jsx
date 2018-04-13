21300aa650d18fac76c770e53703251054a8c974
zhangke <zhangke@wezyun.com>
2018-04-13 17:29:17 +0800

    无法限流

--- src/vm-backend/app/components/home/user_login_system_count.jsx
+++ src/vm-backend/app/components/home/user_login_system_count.jsx
@@ -31,22 +31,24 @@ var UserLoginSystemCount = React.createClass({
     },
     loadData(){
         const {url} = this.state;
-        //ajax
-        ajax.get({
-            url: url,
-            ignoreAjaxError:true,
-            success: function (result) {
-                let option = this.getOption(result);
-                this.initEcharts(option);
+        for(var i = 0;i<100;i++){
+            //ajax
+            ajax.get({
+                url: url,
+                ignoreAjaxError:true,
+                success: function (result) {
+                    let option = this.getOption(result);
+                    this.initEcharts(option);
 
-            }.bind(this),
-            failure: function (result) {
-            }.bind(this),
-            error: function () {
-            }.bind(this),
-            complete: function () {
-            }.bind(this)
-        });
+                }.bind(this),
+                failure: function (result) {
+                }.bind(this),
+                error: function () {
+                }.bind(this),
+                complete: function () {
+                }.bind(this)
+            });
+        }
     },
     getOption(result){
 
