a938a382e0384f1eb9c260ac28e3dc95dc666743
zhangke <zhangke@wezyun.com>
2018-04-23 16:52:00 +0800

    多环境配置正ä增加localhost

--- src/vm-backend/dist/bundle.js
+++ src/vm-backend/dist/bundle.js
@@ -6798,7 +6798,7 @@ module.exports = (__webpack_require__(11))(464);
 "use strict";
 
 
-module.exports = { http_url_prefix: 'http://192.168.0.189:5551' };
+module.exports = { http_url_prefix: 'http://192.168.11.222:5550' };
 // module.exports = { http_url_prefix: 'http://192.168.11.222:5551' };
 // module.exports = { http_uEventEmitterrl_prefix: 'http://47.106.119.0:5551' };
 
